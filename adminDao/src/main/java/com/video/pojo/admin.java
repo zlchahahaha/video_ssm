8e650fcc33896c985fbe9765b4b6d13f181f164e
lyuf <1204851038@qq.com>
2020-10-19 12:51:31 +0800

    create dao pojo

--- adminDao/src/main/java/com/video/pojo/admin.java
+++
@@ -1,8 +0,0 @@
-package com.video.pojo;
-
-/**
- * @author: Administrator
- * @date: 2020/10/19 12:41
- */
-public class admin {
-}
---
+++ dao/SpeakerMapper.java
@@ -0,0 +1,36 @@
+package com.video.dao;
+
+import com.video.pojo.Speaker;
+import com.video.pojo.SpeakerExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SpeakerMapper {
+    int countByExample(SpeakerExample example);
+
+    int deleteByExample(SpeakerExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Speaker record);
+
+    int insertSelective(Speaker record);
+
+    List<Speaker> selectByExampleWithBLOBs(SpeakerExample example);
+
+    List<Speaker> selectByExample(SpeakerExample example);
+
+    Speaker selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Speaker record, @Param("example") SpeakerExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Speaker record, @Param("example") SpeakerExample example);
+
+    int updateByExample(@Param("record") Speaker record, @Param("example") SpeakerExample example);
+
+    int updateByPrimaryKeySelective(Speaker record);
+
+    int updateByPrimaryKeyWithBLOBs(Speaker record);
+
+    int updateByPrimaryKey(Speaker record);
+}
