33c1adea85b35c3c279b673becd74a0b4faf572a
zhang_long_cheng <675673364@qq.com>
2020-10-19 16:29:20 +0800

    create

---
+++ video_service/src/main/java/com/video/listener/LoginListener.java
@@ -0,0 +1,20 @@
+package com.video.listener;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * @author: Administrator
+ * @date: 2020/10/19 16:27
+ */
+public class LoginListener implements HttpSessionListener {
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+
+    }
+}
